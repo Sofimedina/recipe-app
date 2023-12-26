ebaac686350ef85c2d5589b61963267398eb7b30
Karina Sofia Medina <kamedina@expediagroup.com>
2023-12-26 17:48:54 -0300

    query methods example
    Signed-off-by: Karina Sofia Medina <kamedina@expediagroup.com>

--- src/main/java/com/skm/recipeapp/controllers/IndexController.java
+++ src/main/java/com/skm/recipeapp/controllers/IndexController.java
@@ -1,13 +1,20 @@
 package com.skm.recipeapp.controllers;
 
+import com.skm.recipeapp.repositories.CategoryRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
+    private final CategoryRepository categoryRepository;
+
+    public IndexController(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
 
     @RequestMapping({"","/","/index"})
     public String getIndexPage(){
+        System.out.println("Id:"+this.categoryRepository.findByDescription("Italian").get().getId());
         return "index";
     }
 
