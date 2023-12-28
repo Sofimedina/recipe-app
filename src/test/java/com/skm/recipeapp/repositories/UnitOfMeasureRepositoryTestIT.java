a470d181731cb74adfcf5eae558c39c5841adeff
Karina Sofia Medina <kamedina@expediagroup.com>
2023-12-28 17:19:08 -0300

    int test
    Signed-off-by: Karina Sofia Medina <kamedina@expediagroup.com>

---
+++ src/test/java/com/skm/recipeapp/repositories/UnitOfMeasureRepositoryTestIT.java
@@ -0,0 +1,33 @@
+package com.skm.recipeapp.repositories;
+
+import com.skm.recipeapp.domain.UnitOfMeasure;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(SpringExtension.class)
+//@ContextConfiguration()
+@DataJpaTest
+class UnitOfMeasureRepositoryTestIT {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findByDescription() {
+        Optional<UnitOfMeasure> uomOptional=unitOfMeasureRepository.findByDescription("Teaspoon");
+        assertEquals(uomOptional.get().getDescription() ,"Teaspoon");
+
+    }
+}
